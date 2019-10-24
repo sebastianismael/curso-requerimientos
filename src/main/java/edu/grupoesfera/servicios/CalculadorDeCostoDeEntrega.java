9606325e310a4daf5b28c51cf12c596720a1aa52
Sebastian Ismael <sebastian.ismael@grupoesfera.com.ar>
2019-10-24 12:44:34 -0300

    agrega scenario de menos de 4 libros

--- src/main/java/edu/grupoesfera/servicios/CalculadorDeCostoDeEntrega.java
+++ src/main/java/edu/grupoesfera/servicios/CalculadorDeCostoDeEntrega.java
@@ -4,10 +4,21 @@ import edu.grupoesfera.dto.Pedido;
 import edu.grupoesfera.modelo.Entrega;
 
 public class CalculadorDeCostoDeEntrega {
+
+    private static final String VIP = "VIP";
+    private static int MINIMA_CANTIDAD_LIBROS_PARA_ENTREGA_GRATUITA = 5;
+
     public static Integer calcular(Pedido pedido) {
-        if(pedido.getTipoCliente().equalsIgnoreCase("VIP")){
-            return Entrega.GRATUITA.costo();
+        if(esClienteVIP(pedido)){
+            if(pedido.getProductos().get("libros") >= MINIMA_CANTIDAD_LIBROS_PARA_ENTREGA_GRATUITA){
+                return Entrega.GRATUITA.costo();
+            }
         }
         return Entrega.STANDARD.costo();
     }
+
+    private static boolean esClienteVIP(Pedido pedido) {
+        return pedido.getTipoCliente().equalsIgnoreCase(VIP);
+    }
+
 }
