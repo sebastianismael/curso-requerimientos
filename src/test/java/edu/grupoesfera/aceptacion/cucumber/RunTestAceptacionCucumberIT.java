21ed9f76bee1d1c6784e8ee56eb5fef17d9dc6a0
Sebastian Ismael <sebastian.ismael@grupoesfera.com.ar>
2019-11-06 12:14:04 -0300

    ignora el test de smoke

--- src/test/java/edu/grupoesfera/aceptacion/cucumber/RunTestAceptacionCucumberIT.java
+++ src/test/java/edu/grupoesfera/aceptacion/cucumber/RunTestAceptacionCucumberIT.java
@@ -5,7 +5,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources")
+@CucumberOptions(features = "src/test/resources", tags = "not @ignore")
 public class RunTestAceptacionCucumberIT {
 
 }
--- src/test/resources/features/smoke.feature
+++ src/test/resources/features/smoke.feature
@@ -1,5 +1,6 @@
 Feature: Prueba de keep alive sobre el sitio
 
+  @ignore
   Scenario: El sitio esta disponible
     Given
     When hago la prueba de conectividad con el sitio
